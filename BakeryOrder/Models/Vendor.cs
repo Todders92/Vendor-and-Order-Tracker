4f913424ce6d7d3e6458a9b528df3a4ffcfec895
Todd Walraven <cod4247@yahoo.com>
2020-03-13 09:02:38 -0700

    create vendor class

---
+++ BakeryOrder/Models/Vendor.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+
+namespace VendorOrderTracker.Models
+{
+  public class Vendor
+  {
+    private static List<Vendor> _instances = new List<Vendor> { };
+    public string Name { get; set; }
+    public int Id { get; }
+    public List<Order> Orders { get; set; }
+
+    public Vendor(string vendorName)
+    {
+      Name = vendorName;
+      _instances.Add(this);
+      Id = _instances.Count;
+      Orders = new List<Order> { };
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+
+    public static List<Vendor> GetAll()
+    {
+      return _instances;
+    }
+
+    public static Category Find(int serachId)
+    {
+      return _instances[searchId - 1];
+    }
+
+    public void AddOrder(Order order)
+    {
+      Orders.Add(Item);
+    }
+  }
+}
